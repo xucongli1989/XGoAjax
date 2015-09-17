8b4f20dfca45d8b5261ded2010dfe061a8d51f5f
80213876 <80213876@qq.com>
2015-09-17 21:47:24 +0800

    Init

--- Web/XGoAjax.js
+++ Web/XGoAjax.js
@@ -5,7 +5,7 @@
     //默认模板项
     var defaultTemplate = {
         name: "",//模板名
-        before: null,//请求前
+        before: function () { return true;},//请求前，如果返回false，则阻止后续执行。
         error: null,//失败后
         success: null,//成功后
         complete: null,//完成后
@@ -36,15 +36,26 @@
             ops.ajaxOption.type = (ops.ajaxOption.type ? ops.ajaxOption.type : $form.attr("method")) || "POST";
             ops.templateOption = $.extend({}, ops.templateOption, tp.templateOption);
 
-            tp.before.call(this,ops);
+            //返回当前请求状态
+            ops.getState = function () {
+                return null;
+            };
 
-            $.when($.ajax(ops.ajaxOption)).done(function () {
-                tp.success.call(this, ops);
+            if (!tp.before.call(this, ops)) {
+                return false;
+            }
+
+            var dfd=$.when($.ajax(ops.ajaxOption)).done(function (data) {
+                tp.success.call(this, ops,data);
             }).always(function () {
                 tp.complete.call(this, ops);
             }).fail(function () {
                 tp.error.call(this, ops);
             });
+
+            ops.getState = function () {
+                return dfd.state();
+            };
 
         }
     });
@@ -56,3 +67,37 @@
         }
     };
 })(window, document);
+
+
+
+//默认模板
+$.XGoAjax.addTemplate({
+    name: "default",
+    before: function (ops) {
+        return true;
+    },
+    error: function (ops) {
+        console.log("error");
+    },
+    success: function (ops,data) {
+        console.log(ops.getState());
+
+        if (data.Message != "") {
+            console.log(data.Message);
+        }
+
+    },
+    complete: function (ops) {
+        console.log(ops.getState());
+    },
+    templateOption: {
+        //事件触发对象，便于在请求中对其禁用等操作
+        obj: null,
+        //为true，则防止用户再次触发该请求
+        isWaitComplete:true,
+        //请求前要提示的信息
+        beforeSendMsg: "",
+        //true:以alert的方式弹出消息，点确定或关闭执行刷新或其它函数。false:以tips弹出消息
+        isAlertShowMsg: false
+    }
+});
