ea0453719e44876749a8bf47e75f8dc8340314c7
xucl <xucl@Ctrip.com>
2015-09-18 17:47:14 +0800

    Init

---
+++ Web/Data.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class Data : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Threading.Thread.Sleep(3000);
+            Response.Clear();
+            Response.Write(@"{ ""IsSuccess"": true,""Message"": ""保存成功！""}");
+            Response.End();
+        }
+    }
+}
--- Web/XGoAjax.js
+++ Web/XGoAjax.js
@@ -14,25 +14,51 @@
         success: null,
         //完成后
         complete: null,
-        //请求模式，exclusive：独占请求，要想再发起同样的一个请求，必须等待上次请求结束。；greedy：贪婪请求，不限制重复请求
-        mode: "exclusive",
         //模板自定义选项
         templateOption: {}
     };
 
     /*插件默认选项*/
     var defaults = {
+        id:"",
         //模板名
         templateName: "default",
         //模板自定义选项
         templateOption: {},
-        //$.ajax选项
-        ajax: {
+        //请求模式，exclusive：独占请求，要想再发起同样的一个请求，必须等待上次请求结束。；greedy：贪婪请求，不限制重复请求
+        mode: "exclusive",
+        //$.ajax选项，数组的每一项代表一个ajax请求，可以有多个ajax请求
+        ajax: [{
             url: "",
             dataType: "JSON",
             type: "",
             data: null
-        }
+        }]
+    };
+
+    /*ajax请求列表*/
+    var _workList = [];
+    var workItemModel = function (id, promise) {
+        this.id = id;
+        this.promise = promise;
+    };
+    var _addWork = function (item) {
+        _workList.push(item);
+    };
+    var _getWorkById = function (id) {
+        var result = null;
+        $.each(_workList, function (i,n) {
+            if (($.trim(id)).toUpperCase() === n.id.toUpperCase()) {
+                result = n;
+                return false;
+            }
+        });
+        return result;
+    };
+    var _removeById = function (id) {
+        _workList = $.map(_workList, function (n) {
+            return ($.trim(id)).toUpperCase() === id.toUpperCase() ? null : n;
+        });
     };
 
     var $form = $("form:first");
@@ -41,25 +67,49 @@
     $.extend({
         XGoAjax: function (ops) {
             ops = $.extend({}, defaults, ops || {});
-            var dfd = null;
+            var dfd = null, isAllowRun = true;
             var tp = _templates[ops.templateName];//当前模板
-            ops.ajax.url = (ops.ajax.url ? ops.ajax.url : $form.attr("action")) || win.location.href;
-            ops.ajax.data = ops.ajax.data ? ops.ajax.data : $form.serialize();
-            ops.ajax.type = (ops.ajax.type ? ops.ajax.type : $form.attr("method")) || "POST";
+
+            $.each(ops.ajax, function (i,n) {
+                n.url = (n.url ? n.url : $form.attr("action")) || win.location.href;
+                n.data = n.data ? n.data : $form.serialize();
+                n.type = (n.type ? n.type : $form.attr("method")) || "POST";
+            });
+
             ops.templateOption = $.extend({}, ops.templateOption, tp.templateOption);
             
-            if (tp.before.call(this, ops)) {
+            if (!tp.before.call(this, ops)) {
                 //只有当before返回true时，才执行请求
-                dfd = $.when($.ajax(ops.ajax)).done(function (data) {
+                isAllowRun = false;
+            }
+            if (ops.mode === "exclusive") {
+                //独占只能允许一个请求正在执行
+                var item = _getWorkById(ops.id);
+                if (item && item.promise && item.promise.state() === "pending") {
+                    isAllowRun = false;
+                }
+            }
+
+            if (isAllowRun) {
+                var ajaxArr = [];
+                $.each(ops.ajax, function (i,n) {
+                    ajaxArr.push(function () {
+                        return $.ajax(n);
+                    });
+                });
+                dfd = $.when.apply($, ajaxArr);
+                dfd.done(function (data) {
                     if (data && !(data instanceof Object)) {
                         data = $.parseJSON(data);
                     }
                     tp.success.call(this, ops, data);
                 }).always(function () {
+                    _removeById(ops.id);
                     tp.complete.call(this, ops);
                 }).fail(function () {
                     tp.error.call(this, ops);
                 });
+                _addWork(new workItemModel(ops.id, dfd.promise()));
             }
 
             //返回当前请求状态
@@ -77,6 +127,11 @@
             _templates[model.name] = $.extend({}, defaultTemplate, model);
         }
     };
+
+    $.XGoAjax.getAjaxList = function () {
+        return _workList;
+    };
+
 })(window, document);
 
 
@@ -92,18 +147,11 @@ $.XGoAjax.addTemplate({
     },
     success: function (ops, data) {
         console.log("success:" + ops.getState());
-        if (data.Message != "") {
-            console.log(data.Message);
-        }
     },
     complete: function (ops) {
         console.log("complete:" + ops.getState());
     },
     templateOption: {
-        //事件触发对象，便于在请求中对其禁用等操作
-        obj: null,
-        //为true，则防止用户再次触发该请求
-        isWaitComplete: true,
         //请求前要提示的信息
         beforeSendMsg: "正在处理中，请稍后...",
         //true:以alert的方式弹出消息，点确定或关闭执行刷新或其它函数。false:以tips弹出消息
